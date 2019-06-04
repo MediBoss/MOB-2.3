bdc72f573420aa0e4e70d16ffb7229f0ff505188
Medi Assumani <mediassumani49@gmail.com>
2019-06-04 15:33:52 -0700

    [Add] initial code for the image caching exercise

--- GCDPlay.playground/Contents.swift
+++ GCDPlay.playground/Contents.swift
@@ -1,13 +1,55 @@
 import UIKit
+import Foundation
 
-let queue = DispatchQueue(label: "com.makeschool.queue")
+        // ** DISPATCH QUEUE **//
+//DispatchQueue.global().async {
+//    for i in 1...5 {
+//        print("global async 1 : \(i)")
+//    }
+//}
+//
+//DispatchQueue.global().async {
+//    for i in 10...15 {
+//        print("global async 2 : \(i)")
+//    }
+//}
+//
 
-queue.async {
-    for i in 0..<10 {
-        print("🍎 ", i)
-    }
+        // ** DISPATCH WORK ITEM ** //
+
+//let dispatchWorkItem = DispatchWorkItem {
+//
+//    print("start of item")
+//    sleep(2)
+//    print("end of item")
+//
+//}
+//
+//// Runs on the current thread
+//dispatchWorkItem.perform()
+//
+//// Perfrom on the gloabal queue
+//DispatchQueue.global().async(execute: dispatchWorkItem)
+
+
+        //** DISPATCH GROUP **//
+
+let item = DispatchWorkItem {
+    print("work item start")
+    sleep(1)
+    print("work item end")
 }
 
-for i in 100..<110 {
-    print("🐳 ", i)
+let dispatchGroup = DispatchGroup()
+let queue = DispatchQueue(label: "cusrom dq")
+
+queue.async(group: dispatchGroup) {
+    print("block start")
+    sleep(2)
+    print("block end")
+}
+
+DispatchQueue.global().async(group: dispatchGroup, execute: item)
+dispatchGroup.notify(queue: .global()) {
+    print("Dispatch group over")
 }
