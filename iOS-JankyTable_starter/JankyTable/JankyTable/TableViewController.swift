94b5779ad5a7dedf827f77bcd45e02df03cbb7d7
Medi Assumani <mediassumani49@gmail.com>
2019-06-11 18:20:59 -0700

    [Update] completed ios-janky tutrorial

--- iOS-JankyTable_starter/JankyTable/JankyTable/TableViewController.swift
+++ iOS-JankyTable_starter/JankyTable/JankyTable/TableViewController.swift
@@ -6,6 +6,20 @@
 //  Copyright © 2019 Make School. All rights reserved.
 //
 
+
+/**
+ 
+ Questions :
+ 1. 6 threads(inclusive of the main thread)
+ 2. 5 queues
+ 3. Thread 1
+ 4.
+ 5.
+ 6. Because the images are being loaded serially
+ 7. Scrolling is slow because everytime we scroll, the cell is being reused, hence calling the cellForRowAt which is perfoming heaving computation on the main thread
+
+ **/
+
 import UIKit
 
 class TableViewController: UITableViewController {
@@ -16,14 +30,19 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let plist = Bundle.main.url(forResource: "PhotosDictionary", withExtension: "plist"),
-            let contents = try? Data(contentsOf: plist),
-            let serializedPlist = try? PropertyListSerialization.propertyList(from: contents, format: nil),
-            let serialUrls = serializedPlist as? [String: String] else {
-                print("error with serializedPlist")
-                return
+        // Step 1 -  Read and load data from Plist on global queue with a user interactive priority
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            
+            guard let plist = Bundle.main.url(forResource: "PhotosDictionary", withExtension: "plist"),
+                let contents = try? Data(contentsOf: plist),
+                let serializedPlist = try? PropertyListSerialization.propertyList(from: contents, format: nil),
+                let serialUrls = serializedPlist as? [String: String] else {
+                    print("error with serializedPlist")
+                    return
+            }
+            self.photosDict = serialUrls
         }
-        photosDict = serialUrls
     }
     
     override func tableView(_ tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
@@ -36,22 +55,27 @@ class TableViewController: UITableViewController {
         
         var image : UIImage?
         
-        guard let imageURL = URL(string:photos[rowKey] as! String),
-            let imageData = try? Data(contentsOf: imageURL) else {
-                return cell
-        }
-        
-        // Simulate a network wait
-        Thread.sleep(forTimeInterval: 1)
-        print("sleeping 1 sec")
-        
-        let unfilteredImage = UIImage(data:imageData)
-        image = self.applySepiaFilter(unfilteredImage!)
-        
-        // Configure the cell...
-        cell.textLabel?.text = rowKey
-        if image != nil {
-            cell.imageView?.image = image!
+        // Step 2 - Download data and Filter Images concurently on background thread
+        DispatchQueue.global().async {
+            guard let imageURL = URL(string:self.photos[rowKey] as! String),
+                let imageData = try? Data(contentsOf: imageURL) else { return }
+            
+            // Simulate a network wait
+            Thread.sleep(forTimeInterval: 1)
+            print("sleeping 1 sec")
+            
+            let unfilteredImage = UIImage(data:imageData)
+            image = self.applySepiaFilter(unfilteredImage!)
+            
+            // Step 3 -  Update UI on Main Thread
+            DispatchQueue. main.async {
+                // Configure the cell...
+                cell.textLabel?.text = rowKey
+                if image != nil {
+                    cell.imageView?.image = image!
+                }
+            }
+            
         }
         return cell
     }
