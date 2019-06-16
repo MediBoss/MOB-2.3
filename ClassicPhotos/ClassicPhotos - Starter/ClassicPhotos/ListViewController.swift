1df799fbd59cbf7d6a97727fcf6c8e3a119872e0
Medi Assumani <mediassumani49@gmail.com>
2019-06-16 12:19:23 -0700

    minor change

--- ClassicPhotos/ClassicPhotos - Starter/ClassicPhotos/ListViewController.swift
+++ ClassicPhotos/ClassicPhotos - Starter/ClassicPhotos/ListViewController.swift
@@ -50,6 +50,7 @@ class ListViewController: UITableViewController {
   }
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
         
         //1
