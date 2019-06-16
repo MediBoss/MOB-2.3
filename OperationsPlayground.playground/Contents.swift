c3310daf5c0fbde74147f219ec5093e9acb6cb1e
Medi Assumani <mediassumani49@gmail.com>
2019-06-16 11:05:03 -0700

    [Add] initial work on RayWenderleich Operations tutorial

--- OperationsPlayground.playground/Contents.swift
+++ OperationsPlayground.playground/Contents.swift
@@ -1,28 +1,30 @@
   import Foundation
   
-  let printerOperation = BlockOperation() // 1) create printerOperation as BlockOperation
-
-  // 2) add code blocks to the operation
-  printerOperation.addExecutionBlock { print("I") }
-  printerOperation.addExecutionBlock { print("am") }
-  printerOperation.addExecutionBlock { print("printing") }
-  printerOperation.addExecutionBlock { print("block") }
-  printerOperation.addExecutionBlock { print("operation") }
-  printerOperation.addExecutionBlock { print("Medi") }
-
-  printerOperation.completionBlock = { // 3) set completion block
-    print("I'm done printing")
-    // do something here once all the blocks have complete
-  }
-
-  let operationQueue = OperationQueue() // 4) Create an OperationQueue
-  operationQueue.addOperation(printerOperation) // 5) add operation to queue
-
-  
+//  let printerOperation = BlockOperation() // 1) create printerOperation as BlockOperation
+//
+//  // 2) add code blocks to the operation
+//  printerOperation.addExecutionBlock { print("I") }
+//  printerOperation.addExecutionBlock { print("am") }
+//  printerOperation.addExecutionBlock { print("printing") }
+//  printerOperation.addExecutionBlock { print("block") }
+//  printerOperation.addExecutionBlock { print("operation") }
+//  printerOperation.addExecutionBlock { print("Medi") }
+//
+//  printerOperation.completionBlock = { // 3) set completion block
+//    print("I'm done printing")
+//    // do something here once all the blocks have complete
+//  }
+//
+//  let operationQueue = OperationQueue() // 4) Create an OperationQueue
+//  operationQueue.addOperation(printerOperation) // 5) add operation to queue
+//
+//
          // TESTING OPERATION BLOCK ORDER OF EXECUTION
 
   let complexOperation = BlockOperation()
   let complexOperationQueue = OperationQueue()
+  
+  complexOperationQueue.maxConcurrentOperationCount = 2 // Num of ops to run concurently
 
 
   // First block
@@ -49,5 +51,5 @@
     print("block 3 done")
   }
 
-
+  //OperationQueue.main // To do UI work
   complexOperationQueue.addOperation(complexOperation)
